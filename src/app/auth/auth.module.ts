58526169cbe8e342c496e5fb9370cf329a0eb50e
Rodion Sibov <rodionsibov@gmail.com>
2022-04-20 00:44:41 +0300

    first commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
+
+@NgModule({
+  declarations: [RegisterComponent, LoginComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+})
+export class AuthModule {}
---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+    console.log('invalid', this.form.invalid);
+    console.log('valid', this.form.valid);
+    console.log('errors', this.form.get('email')?.hasError('required'));
+  }
+}
